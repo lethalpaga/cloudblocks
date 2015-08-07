8158dfde27853470eba158e7b02752bd283ad745
lethalpaga <lethalpaga@gmail.com>
2015-08-07 17:00:21 +1200

    Refactor BasePlate to use a proper class instance

--- lib/cloudbricks/base_plate.rb
+++ lib/cloudbricks/base_plate.rb
@@ -5,7 +5,17 @@ module CloudBricks
   # the references to all {Base} objects
   module BasePlate
 
+    class NestedError < StandardError; end
+
     module_function
+
+    def base_plate
+      @@current_base_plate
+    end
+
+    def _set_current_base_plate(plate)
+      @@current_base_plate = plate
+    end
 
     def objects
       @objects
@@ -14,7 +24,24 @@ module CloudBricks
     def create(name = nil, &block)
       @objects = {}
 
+      BasePlateImpl.new(name, &block)
+
       instance_eval(&block)
+    end
+
+    class BasePlateImpl < Base
+      def initialize(name, &block)
+        @backtrace = backtrace
+
+        fail NestedError.new("BasePlate already defined at #{base_plate.show_backtrace}") unless base_plate.nil?
+        _set_current_base_plate(self)
+
+        super(name, &block)
+      end
+
+      def backtrace
+        caller
+      end
     end
 
     # Defines a brick
